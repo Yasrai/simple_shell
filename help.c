32da52f954640fc9c0f4489978a066fbd159d692
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-07-24 13:51:04 -0700

    simple_shell

---
+++ help.c
@@ -0,0 +1,47 @@
+#include "simpleshell.h"
+
+/**
+ * env - Help information for the builtin env
+ * Return: no return
+ */
+
+void env(void)
+{
+	char *help = "env: env [option] [name=value] [command [args]]\n\t";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Print the enviroment of the shell.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+
+}
+
+/**
+ * setenv - Help information for the builtin setenv
+ * Return: no return
+ */
+
+void setenv(void)
+{
+
+	char *help = "setenv: setenv (const char *name, const char *value,";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "int replace)\n\t";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Add a new definition to the environment\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
+/**
+ * unsetenv - Help information for the builtin unsetenv
+ * Return: no return
+*/
+
+void unsetenv(void)
+{
+	char *help = "unsetenv: unsetenv (const char *name)\n\t";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Remove an entry completely from the environment\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
